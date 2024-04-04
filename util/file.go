53f55f00c9d052187a8e7614af794da42197d363
tangming <tangming@nibirutech.com>
2024-04-05 01:46:27 +0800

    1

---
+++ util/file.go
@@ -0,0 +1,19 @@
+package util
+
+import (
+	"io"
+	"nett/zlog"
+	"os"
+)
+
+func ReadFile(name string) []byte {
+	file, err := os.OpenFile(name, os.O_RDONLY, 0)
+	if err != nil {
+		zlog.Panic(err)
+	}
+	b, err := io.ReadAll(file)
+	if err != nil {
+		zlog.Panic(err)
+	}
+	return b
+}
