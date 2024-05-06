40e5f277c29a3d7e7664a6bf3eb2ecd5c106878c
tangming <tangming@nibirutech.com>
2024-05-06 12:03:15 +0800

    1

--- msgbus/register.go
+++ msgbus/register.go
@@ -8,24 +8,24 @@ import (
 	"github.com/tnnmigga/core/infra/zlog"
 )
 
-func RegisterHandler[T any](m idef.IModule, fn func(T)) {
+func RegisterHandler[T any](m idef.IModule, fn func(*T)) {
 	var tmp T
-	mType := reflect.TypeOf(tmp)
+	mType := reflect.TypeOf(&tmp)
 	codec.Register[T]()
 	registerRecver(mType, m)
 	m.RegisterHandler(mType, func(data any) {
-		msg := data.(T)
+		msg := data.(*T)
 		fn(msg)
 	})
 }
 
-func RegisterRPC[T any](m idef.IModule, fn func(msg T, resolve func(any), reject func(error))) {
+func RegisterRPC[T any](m idef.IModule, fn func(msg *T, resolve func(any), reject func(error))) {
 	var tmp T
-	mType := reflect.TypeOf(tmp)
+	mType := reflect.TypeOf(&tmp)
 	codec.Register[T]()
 	registerRecver(mType, m)
 	m.RegisterHandler(mType, func(data any, res func(any), rej func(error)) {
-		msg := data.(T)
+		msg := data.(*T)
 		fn(msg, res, rej)
 	})
 }
